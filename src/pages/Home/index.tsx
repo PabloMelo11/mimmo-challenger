d80c616192fd0d30087b9102b66ec97405bccad4
PaabloMelo <pablo.melo@tangotech.com.br>
2020-10-09 00:36:39 -0300

    chore: importing external sources

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  TitleMimo,
+  TextWelcome,
+  TextSub,
+  TextYourself,
+} from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Text>Teste</Text>
+      <Content>
+        <TitleMimo>mimo</TitleMimo>
+
+        <TextWelcome>Bem vindo à Mimoo!</TextWelcome>
+
+        <TextSub>Alegre. Divertido. Relevante. Você vai adorar!</TextSub>
+
+        <TextYourself>
+          Nos conte um pouco sobre você e ganhe pontos!
+        </TextYourself>
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  padding: 42px 23px;
+`;
+
+export const TitleMimo = styled.Text`
+  color: #00c3a3;
+  font-size: 35px;
+  font-family: 'Poppins-Bold';
+  margin-bottom: 23px;
+`;
+
+export const TextWelcome = styled.Text`
+  color: #00c3a3;
+  font-size: 35px;
+  font-family: 'Poppins-Bold';
+`;
+
+export const TextSub = styled.Text`
+  color: #00c3a3;
+  font-size: 20px;
+  font-family: 'Poppins-SemiBold';
+  margin-top: 21px;
+`;
+
+export const TextYourself = styled.Text`
+  color: #3a3a3a;
+  font-size: 20px;
+  font-family: 'Poppins-Regular';
+  margin-top: 21px;
+`;
